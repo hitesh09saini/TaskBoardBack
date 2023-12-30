087543b831ac91c5c0f872c5b2b81c4a60077c46
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-30 19:17:31 +0530

    push

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cors({
-	origin: 'https://task-boardbyhitesh.netlify.app/',
-	credentials: true
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
